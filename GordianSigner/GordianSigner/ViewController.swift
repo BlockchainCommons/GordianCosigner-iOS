42b9febf8a1df5ba995bfa5497358b6cdc963f02
Fonta1n3 <fontainedenton@googlemail.com>
2020-09-29 11:32:42 +0800

    feat: add Xcode project

---
+++ GordianSigner/GordianSigner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GordianSigner
+//
+//  Created by Peter on 9/29/20.
+//  Copyright © 2020 Blockchain Commons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
